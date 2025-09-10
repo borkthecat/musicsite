83af6abc0dda1f8e7d0d340f52d516a4c0f0ce6d
borkthecat <readersfiction@gmail.com>
2025-09-11 03:56:46 +0800

    update asset paths for images and songs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
+  base: '/musicsite/',
+
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [mdx()]
+  integrations: [mdx()],
 });
